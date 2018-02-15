4f7d77c894ce59df4f6ebd1fb4ec50b4cca2514e
lmccloskey19 <33226611+lmccloskey19@users.noreply.github.com>
2018-02-15 09:19:17 -0600

    Update sensestop.py

--- sensestop.py
+++ sensestop.py
@@ -2,13 +2,14 @@ import RoboPiLib as RPL
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
 def servomove(x):
-    if x == 1:
-        RPL.servoWrite(0, 2000)
-        RPL.servoWrite(1, 1000)
     if x == 0:
         RPL.servoWrite(0, 0)
         RPL.servoWrite(1, 0)
 
+    if x == 1:
+        RPL.servoWrite(0, 2000)
+        RPL.servoWrite(1, 1000)
+        
 RPL.pinMode(16, RPL.INPUT)
 
 while True:
@@ -16,6 +17,4 @@ while True:
     if sensorRead == 1:
         servomove(1)
     else:
-        servomove(0)
-        RPL.servoWrite(0, 1000)
-        RPL.servoWrite(1, 2000)
+        servomove(0
